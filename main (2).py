de009c2041cc5b1adf84017251ef016dc4313aef
TsowmiyaT <144543028+TsowmiyaT@users.noreply.github.com>
2023-09-09 20:12:55 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,14 @@
+#challenge2
+
+def CheckLeap(Year):
+
+  if((Year % 400 == 0)or
+     (Year % 100 != 0)and
+     (Year % 4 ==0)):
+
+    print("Given year is a leap year")
+  else:
+    print("Given year is not leap year")
+
+Year=int(input("Enter the number"))
+CheckLeap(Year)
